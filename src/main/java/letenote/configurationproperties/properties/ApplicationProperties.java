d0a1b09b5120dbc9aa46b59c5bcb68dbf6863759
aditReza <lete.note3@gmail.com>
2022-05-16 17:38:10 +0700

    implement confiration properties

---
+++ src/main/java/letenote/configurationproperties/properties/ApplicationProperties.java
@@ -0,0 +1,14 @@
+package letenote.configurationproperties.properties;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@Getter
+@Setter
+@ConfigurationProperties("application")
+public class ApplicationProperties {
+	private String name;
+	private Integer version;
+	private boolean productionMode;
+}
