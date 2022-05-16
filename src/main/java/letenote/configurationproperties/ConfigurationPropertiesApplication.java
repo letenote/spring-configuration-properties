c0e6f75d558093cd7d7a60496c8095f838c01461
aditReza <lete.note3@gmail.com>
2022-05-16 17:25:41 +0700

    app init

---
+++ src/main/java/letenote/configurationproperties/ConfigurationPropertiesApplication.java
@@ -0,0 +1,13 @@
+package letenote.configurationproperties;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigurationPropertiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigurationPropertiesApplication.class, args);
+	}
+
+}
